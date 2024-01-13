da1f26fb51a0fcca3e578e4e8733660f57ca8277
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-01-14 00:35:41 +0100

    urls.py aktualisieren

--- account/urls.py
+++ account/urls.py
@@ -9,11 +9,12 @@ SignupView,
 
 
 urlpatterns = [
+    path('register/', register, name='register'),
     path('password/change/',PasswordChangeView.as_view(), name='password_change'),
     path('password/reset/done/',PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password/reset/',PasswordResetView.as_view(), name='password_reset'),
     path('login/',LoginView.as_view(), name='login'),
     path('logout/',LogoutView.as_view(), name='logout'),
     path('signup/',SignupView.as_view(), name='signup_view'),
-    path('register/', register, name='register'),
+    
 ]
